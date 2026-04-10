fadc5be07fe2e1ff0b371ab27987ebc8941ae68a
Laura Weber <weber42@llnl.gov>
2026-04-10 15:31:35 -0700

    Add caching for dependency info data collection (may resolve timeouts)

--- _visualize/scripts/cache_repos_dependencies.py
+++ _visualize/scripts/cache_repos_dependencies.py
@@ -1,9 +1,8 @@
 from scraper.github import queryManager as qm
 from os import environ as env
-import re
 
 ghDataDir = env.get("GITHUB_DATA", "../github-data")
-query_in = "/repos/OWNNAME/REPONAME/stats/commit_activity"
+queryPath = "../queries/repo-Dependencies.gql"
 
 # Read repo info data file (to use as repo list)
 inputLists = qm.DataManager("%s/intReposInfo.json" % ghDataDir, True)
@@ -17,17 +16,25 @@ print("Repo list complete. Found %d repos." % (len(repolist)))
 queryMan = qm.GitHubQueryManager(maxRetry=1, retryDelay=1)
 
 # Iterate through internal repos
-print("Gathering data across multiple queries...")
+print("Gathering data across multiple paginated queries...")
 for repo in repolist:
     print("\n'%s'" % (repo))
 
     r = repo.split("/")
-
-    gitquery = re.sub("OWNNAME", r[0], query_in)
-    gitquery = re.sub("REPONAME", r[1], gitquery)
-
     try:
-        outObj = queryMan.queryGitHub(gitquery, rest=True)
+        outObj = queryMan.queryGitHubFromFile(
+            queryPath,
+            {
+                "ownName": r[0],
+                "repoName": r[1],
+                "numManifests": 25,
+                "numDependents": 100,
+                "pgCursor": None,
+            },
+            paginate=True,
+            cursorVar="pgCursor",
+            keysToList=["data", "repository", "dependencyGraphManifests", "nodes"],
+        )
     except Exception as error:
         print("Warning: Could not complete '%s'" % (repo))
         print(error)
@@ -35,5 +42,6 @@ for repo in repolist:
 
     print("'%s' Done!" % (repo))
 
-print("\nCache requests complete!")
+print("\nCollective data gathering complete!")
+
 print("\nDone!\n")
